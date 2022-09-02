5fa1e41bb87e848ca068b2a255b6553e4a6056ad
yurivalladares <yuri@valladares.com.br>
2022-09-02 10:16:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import src.settings
 from src.conditions import is_palindrome, is_prime
 from src.searchapi import search_pi_api
-from src.searchfile import *
 
 def main():
-    # conditions with smaller time complexity should come first to benefit from short circuit evaluation
+
+    #Hint: At condition_list, conditions with smaller time complexity should come first to benefit from short circuit evaluation
+
     search_pi_api(
-        start=1,
+        start_position=1,
         digits=7,
+        reverse=False,
         condition_list=[
             is_palindrome,
             is_prime
-        ])
+        ]
+    )
 
 if __name__ == '__main__':
     main()
+
