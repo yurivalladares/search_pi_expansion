a2fa5cc2fcd0cef84434f9a8958c1b68bab500b7
yurivalladares <yuri@valladares.com.br>
2022-09-02 10:35:33 -0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -2,18 +2,21 @@ import logging
 from datetime import datetime
 import os
 
-path = os.path.join('./', 'logs')
+path = os.path.join("./", "logs")
 
 if not os.path.isdir(path):
     os.mkdir(path)
 
 
 logging.basicConfig(
-        encoding='utf-8',
-        level=logging.INFO,
-        format='%(asctime)s %(message)s',
-        datefmt="%Y-%m-%d %H:%M:%S",
-        handlers=[
-        logging.FileHandler(datetime.now().strftime("./logs/search_log_%Y-%m-%d_%H-%M-%S.log")),
-        logging.StreamHandler()
-    ])
+    encoding="utf-8",
+    level=logging.INFO,
+    format="%(asctime)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler(
+            datetime.now().strftime("./logs/search_log_%Y-%m-%d_%H-%M-%S.log")
+        ),
+        logging.StreamHandler(),
+    ],
+)
